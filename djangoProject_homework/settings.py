f980c531d6d4986d0d300f772853b3804057f464
ryfe <denis.koptelev@gmail.com>
2024-03-04 22:12:47 +0300

    Update project. Create models, upload settings

--- djangoProject_homework/settings.py
+++ djangoProject_homework/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject_homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'skyproducts',
+        'USER': 'postgres',
+        'PASSWORD': 8953,
+        'HOST': '127.0.0.1',
+        'PORT': 5432
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def str(self):
+        return self.name
+
+    class Meta(models.Model):
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    img = models.ImageField(upload_to='poducts/', verbose_name='Фотография', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+
+    def str(self):
+        return self.name
+
+    class Meta(models.Model):
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
