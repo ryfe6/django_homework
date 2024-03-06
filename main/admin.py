fb070ee9d982c90b67b893f16bcc012a0225b438
ryfe <denis.koptelev@gmail.com>
2024-03-06 20:06:44 +0300

    Update project. Update models. Create custom comand, clear BD and new write data.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Category, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'pk',)
+
+
+
--- main/models.py
+++ main/models.py
@@ -1,33 +1,35 @@
 from django.db import models
 
-
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-
-    def str(self):
-        return self.name
-
-    class Meta(models.Model):
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
-
-
 class Product(models.Model):
+    """product model работает с продуктами"""
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     img = models.ImageField(upload_to='poducts/', verbose_name='Фотография', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", related_name='Products', verbose_name="Категория",
+                                 on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата последнего изменения')
 
-    def str(self):
+    def __str__(self):
         return self.name
 
-    class Meta(models.Model):
+    class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    """category model работает с категориями товаров"""
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
