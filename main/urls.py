b5f3fbb21a849293a70d5fd2a071b187bedd4099
ryfe <denis.koptelev@gmail.com>
2024-03-15 11:56:40 +0300

    update homework 3

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from main.views import product_list, contacts, product_view
+from main.apps import MainConfig
 
-from main.views import product_list, contacts
-
+app_name = MainConfig.name
 
 urlpatterns = [
-    path('', product_list),
-    path("contacts/", contacts)
-    ]
+    path('', product_list, name='product_list'),
+    path("contacts/", contacts, name='contacts'),
+    path('main/<int:pk>/', product_view, name='product_view'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from main.models import Product
 
@@ -7,13 +7,21 @@ from main.models import Product
 
 
 def product_list(request):
+    """Вывод шаблона с информацией о продукте"""
     items = Product.objects.all()
     context = {'object_list': items}
     return render(request, 'main/product_list.html', context)
 
 
 def contacts(request):
+    """Вывод шаблона с контактной информацией"""
     return render(request, 'main/contacts.html')
+
+
+def product_view(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    context = {'item': item}
+    return render(request, 'main/product_view.html', context)
 
 
 def students_list(request, *args, **kwargs):
